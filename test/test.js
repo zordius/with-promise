5b316383b65c6d3104cd8abef663ecff68c60406
Zordius Chen <zordius@yahoo-inc.com>
2014-12-25 19:49:42 +0800

    more tests

--- test/test.js
+++ test/test.js
@@ -5,9 +5,18 @@ var assert = require('chai').assert,
 
 describe('WithPromise', function () {
     it('should be a promise constructor', function (done) {
-        var P = new WithPromise(function () {});
+        var P = WithPromise.create(function () {});
 
         assert.equal('object', typeof P);
+        assert.equal('function', typeof P.then);
+        assert.equal('function', typeof P['catch']);
         done();
+    });
+
+    it('should call executor with context', function (done) {
+        WithPromise.create(function (resolve, reject) {
+            assert.equal(1, this.a);
+            done();
+        }, {a: 1});
     });
 });
