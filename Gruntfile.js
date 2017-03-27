f0ffce1b20a7c6809483f63dd4e8168ac2ffbd96
Zordius Chen <zordius@yahoo-inc.com>
2017-03-27 11:11:06 +0800

    try to make saucelabs stable

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,9 @@ module.exports = function (grunt) {
                     // tunnelArgs: ['-B', 'www.google.com'],
                     detailedError: true,
                     concurrency: 2,
+                    throttled: 6,
                     maxPollRetries: 3,
+                    maxRetries: 2,
                     'max-duration': 300,
                     browsers: [
                         {browserName: 'chrome'},
