565d63c6e03c1273a8598f1ee7dcff281de0d0de
Zordius Chen <zordius@yahoo-inc.com>
2017-03-28 09:43:54 +0800

    push with auth

--- travis.sh
+++ travis.sh
@@ -49,7 +49,7 @@ cp node_modules/mocha/mocha.js testdist/
 cp node_modules/mocha/mocha.css testdist/
 git add index.html testdist/
 git commit -m "New tests on github"
-git push origin gh-pages
+git push "https://${GHTK}@github.com/zordius/with-promise.git" gh-pages > /dev/null 2>&1
 git checkout dist_ready
 
 # wait github update gh-pages
