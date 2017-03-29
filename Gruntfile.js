7e7121a6fe62f12fe739be694182463d27fb83bc
Zordius Chen <zordius@yahoo-inc.com>
2017-03-29 13:06:51 +0800

    more iphone

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,11 @@ module.exports = function (grunt) {
                         {browserName: 'internet explorer', version: 11, platform: 'Windows 8.1'},
                         {browserName: 'internet explorer', version: 10, platform: 'Windows 8'},
                         {browserName: 'internet explorer', version: 9, platform: 'Windows 7'},
-                        {browserName: 'iphone', version:'8.1', platform: 'OS X 10.10'}
+                        {browserName: 'iphone', version:'10.2', platform: 'OS X 10.10'},
+                        {browserName: 'iphone', version:'9.3', platform: 'OS X 10.10'},
+                        {browserName: 'iphone', version:'8.4', platform: 'OS X 10.10'},
+                        {browserName: 'iphone', version:'7.1', platform: 'OS X 10.10'}
+                        {browserName: 'iphone', version:'6.1', platform: 'OS X 10.8'}
                     ]
                 }
             }
