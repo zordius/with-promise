156ab7f3f74f04c8790354f33618d13492688965
Zordius Chen <zordius@yahoo-inc.com>
2017-03-29 14:29:55 +0800

    add ipad back

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,9 @@ module.exports = function (grunt) {
                         {browserName: 'internet explorer', version: 11, platform: 'Windows 8.1'},
                         {browserName: 'internet explorer', version: 10, platform: 'Windows 8'},
                         {browserName: 'internet explorer', version: 9, platform: 'Windows 7'},
+                        {browserName: 'ipad', version:'10.2', platform: 'OS X 10.10'},
+                        {browserName: 'ipad', version:'9.3', platform: 'OS X 10.10'},
+                        {browserName: 'ipad', version:'8.4', platform: 'OS X 10.10'},
                         {browserName: 'iphone', version:'10.2', platform: 'OS X 10.10'},
                         {browserName: 'iphone', version:'9.3', platform: 'OS X 10.10'},
                         {browserName: 'iphone', version:'8.4', platform: 'OS X 10.10'}
